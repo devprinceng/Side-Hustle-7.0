66a489967bc6844a600876923ddc3ef06c85b612
devprinceng <devprinceng@gmail.com>
2023-03-14 01:19:55 +0100

    Side Hustle 7.0 Week 2 Task

--- voter_validator.php
+++ voter_validator.php
@@ -8,32 +8,36 @@
 
     //this function accepts voters age,a true or false value for voter_has_pvc and voters ward(numeric string)
     function validateVoter($voter_age, $voter_has_pvc, $voter_ward){
-        //lets checks if all 3 validation conditions passed
-        if ($voter_age >= 18 && $voter_has_pvc == true && $voter_ward == "020"){
+        //check if all 3 validation conditions passed
+        if ($voter_age >= 18 && $voter_has_pvc && $voter_ward == "020"){
             // all 3 conditions passed
-            echo "VOTER ELIGIBLE TO VOTE! <br/><br/>"; // 
+            echo "<br>VOTER ELIGIBLE TO VOTE! <br/><br/>";
         }else{
             // validation above didn't pass so lets find which of them failed,
             // and display a corresponding message to the user.
-            echo "OOPS!!! YOU'RE NOT ELIGIBLE TO VOTE! <br/>REASONS: <br/>";
+            echo "<br>OOPS!!! YOU'RE NOT ELIGIBLE TO VOTE! <br/>REASONS: <br/>";
             if($voter_age < 18 ){
                 echo "You must be 18years and above! <br/>";
             }
-            if($voter_has_pvc == false){
-                echo "You don't have a PVC<br/>";
+            if(!$voter_has_pvc){ //voter has no pvc
+                echo "You don't have a PVC <br/>";
             }
             if ($voter_ward != "020"){
-                echo "Only Ward 020 voters are eligible to vote here </b>";
+                echo "Only Ward 020 voters are eligible to vote here <br/>";
             }
         }
     }
 	
-    //lets create 2 associative arrays to hold voter1 and voter2 details respectively
+    //lets create 2 associative arrays to hold voter1, voter2 & voter3 details respectively
     $voter1 = array('age'=> 26,'has_pvc'=>true, 'ward'=>"020");
-    $voter2 = array('age'=> 14,'has_pvc'=>false, 'ward'=>"010");
+    $voter2 = array('age'=> 14,'has_pvc'=>true, 'ward'=>"010");
+    $voter3 = array('age'=> 18,'has_pvc'=>false, 'ward'=>"");
+    $voter4 = array('age'=> 21,'has_pvc'=>true, 'ward'=>"020");
 
-    //lets call our function & pass voter1 and voter2 details as arguments respectively.
+    //lets call our function & pass voter1,voter2 & voter3 details as arguments respectively.
     validateVoter($voter1['age'], $voter1['has_pvc'], $voter1['ward']);// eligible to vote
     validateVoter($voter2['age'], $voter2['has_pvc'], $voter2['ward']);// not eligible to vote
+    validateVoter($voter3['age'], $voter3['has_pvc'], $voter3['ward']);// not eligible to vote
+    validateVoter($voter4['age'], $voter4['has_pvc'], $voter4['ward']);// eligible to vote
 
 ?>
